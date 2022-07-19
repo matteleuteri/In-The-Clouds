9d51d941591167a6a6c04e6b1a02c8201d8e34d0
Matthew Eleuteri <meleuteri2@gmail.com>
2022-07-18 22:45:00 -0400

    '.'

--- code/WorldChunk.cpp
+++ code/WorldChunk.cpp
@@ -1,14 +1,16 @@
 #include "headers/WorldChunk.h"
 
+#define X_DIFF_CONST 400
+
 WorldChunk::WorldChunk(AnimationController *animationController, float x, float y, float xSpeed): GameObject(animationController, x, y, xSpeed)
 {
-    xMax = x + 400;
-    xMin = x - 300;
+    xMax = x + X_DIFF_CONST;
+    xMin = x - X_DIFF_CONST;
 }
 
 void WorldChunk::update(int32_t timeElapsed)
 {
-    x += (xSpeed * timeElapsed / 15); // whats going on here
+    x += (xSpeed * timeElapsed / 15);
     if(x > xMax || x < xMin) 
     {
         xSpeed *= -1;
