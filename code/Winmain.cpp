1adbc3335a4e2465f52a69ada3cf7276b281cfe5
Matthew Eleuteri <meleuteri2@gmail.com>
2022-07-24 22:44:01 -0400

    .

--- code/Winmain.cpp
+++ code/Winmain.cpp
@@ -233,6 +233,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
                         chunk3AnimationController, backgroundAnimationController, cloudLayer1AnimationController, cloudLayer2AnimationController };
 
             scene = std::make_unique<Scene>(GetTickCount(), true, animationControllers, 0.0f, 0.0f, &rc);
+            
+            
+            
+            // here is where I can mess with screen resolution stuff
             scene->width = (float)rc.right - rc.left;
 
             up_Button = new JumpButton();
